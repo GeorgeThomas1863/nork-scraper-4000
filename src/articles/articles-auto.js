85ac60398f9496f7135a4b9103228e43167710ad
George H. Thomas <georgebushdontcare@gmail.com>
2025-04-08 21:18:11 -0400

    commit before dumb refactor

--- src/articles/articles-auto.js
+++ src/articles/articles-auto.js
@@ -1,12 +1,40 @@
-import { getArticleListHtml } from "./articles-get";
+import { getArticleListHtml, getArticleArray } from "./articles-get";
 import { parseArticleListHtml } from "./articles-parse";
 import { storeArticleListArray } from "./articles-store";
 
-export const getNewArticles = async () => {
+export const runArticleScrape = async () => {
+    //get / find new articles
+    const newArticleURLs  = await getNewArticleURLs()
+    //check if any new, return null if not
+    if (!newArticleURLs || newArticleURLs.length === 0) return null
+    //otherwise download new articles
+    await downloadNewArticles(newArticleURLs)
+}
+
+
+export const getNewArticleURLs = async () => {
   //gets html from page with current list of articles
   const articleListHtml = await getArticleListHtml();
 
   //get the article list array from current articles html
   const articleListArray = await parseArticleListHtml(articleListHtml);
   await storeArticleListArray(articleListArray); //store the article list
+
+  //identifies new urls NOT downloaded
+  const newArticleURLs = await getArticleArray("articlesToDownload");
+  return newArticleURLs
 };
+
+//input is array of objects
+export const downloadNewArticles = async (inputArray) => {
+    
+    //return if input empty (shouldnt happen)
+    if (!inputArray || inputArray.length === 0) return
+
+    //loop through input array
+    for (let i = 0; i < inputArray.length; i++) {
+        const article = inputArray[i].url
+    }
+
+
+}
