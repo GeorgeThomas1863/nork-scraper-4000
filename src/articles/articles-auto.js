07c1b12593e52a843a7e97294b28d44147a939fb
George H. Thomas <georgebushdontcare@gmail.com>
2025-04-09 09:11:57 -0400

    gonna be late

--- src/articles/articles-auto.js
+++ src/articles/articles-auto.js
@@ -3,6 +3,8 @@ import { getNewArticleURLs, getNewArticleData } from "./articles-get.js";
 export const runArticleScrape = async () => {
   //get / find new articles
   const newArticleURLs = await getNewArticleURLs();
+  console.log(newArticleURLs);
+  return;
   //check if any new, return null if not
   if (!newArticleURLs || newArticleURLs.length === 0) return null;
   //otherwise download new articles
