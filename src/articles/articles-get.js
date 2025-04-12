35ade8cd7c9a15048fbff22ea60ec19d2388042f
George H. Thomas <georgebushdontcare@gmail.com>
2025-04-11 20:36:20 -0400

    bugs

--- src/articles/articles-get.js
+++ src/articles/articles-get.js
@@ -3,7 +3,7 @@ import KCNA from "../../models/kcna-model.js";
 import dbModel from "../../models/db-model.js";
 
 import { parseArticleListHtml, parseArticleContentHtml } from "./articles-parse.js";
-// import { storeArticleArray } from "./articles-store.js";
+import { downloadPicsFS } from "../pics/pics-download.js";
 
 export const getNewArticleURLs = async () => {
   //gets html from page with current list of articles
@@ -55,41 +55,3 @@ export const getNewArticleData = async (inputArray) => {
   }
   return true;
 };
-
-// //seperate to avoid fucking up url key in other equation
-// export const getArticlePicHtml = async (picURL) => {
-//   const articlePicModel = new KCNA({ url: picURL });
-//   const articlePicHtml = await articlePicModel.getHTML();
-//   return articlePicHtml;
-// };
-
-// /**
-//  * GETs article array of ONLY NEW SHIT (urls) for specified type; so always downloading / uploading new stuff, skipping already done stuff
-//  * @function getArticleArray
-//  * @param {string} type - Type of articles to get ("articlesToDownload" or "articlesToUpload")
-//  * @returns {Promise<Array>} Array of article objects
-//  */
-// export const getArticleArray = async (type) => {
-//   let params = "";
-
-//   //return on error input
-//   if (type !== "articlesToDownload" && type !== "articlesToUpload") return;
-
-//   if (type === "articlesToDownload") {
-//     params = {
-//       collection1: CONFIG.articleListCollection, //old thing, to compare against
-//       collection2: CONFIG.articleContentCollection, //new thing, what this funct is doing
-//     };
-//   }
-
-//   if (type === "articlesToUpload") {
-//     params = {
-//       collection1: CONFIG.articleContentCollection,
-//       collection2: CONFIG.articlePostedCollection,
-//     };
-//   }
-
-//   const articleModel = new dbModel(params, "");
-//   const articleArray = await articleModel.findNewURLs();
-//   return articleArray;
-// };
